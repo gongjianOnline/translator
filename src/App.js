0d391584f8aedeee69ad2238a88c6e14dfd0533a
李承斌 <15031617367@163.com>
2020-10-30 11:13:06 +0800

    引入了antd组件

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Translate from "./view/translate/translate"
 
 import './App.css';
+import 'antd/dist/antd.css';
 import React from "react";
 
 function App() {
--- src/view/translate/translate.js
+++ src/view/translate/translate.js
@@ -1,8 +1,22 @@
 import React from "react"
+import "./translate.css"
+import { Input } from 'antd';
+const { Search } = Input;
 const  translate = ()=>{
+  let onSearch = ()=>{
+    console.log("123456")
+  };
   return (
-    <div>
-      
+    <div className="translate-wrapper">
+      <div className="search-container">
+        <Search
+          placeholder="请输入查询的参数"
+          allowClear
+          enterButton="查询"
+          size="large"
+          onSearch={()=>onSearch()}
+        />
+      </div>
     </div>
   )
 }
