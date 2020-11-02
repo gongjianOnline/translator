e92451408365da5835b7dbfb63fc2eda48d86f2b
李承斌 <15031617367@163.com>
2020-11-02 13:48:16 +0800

    简单版英译汉

--- src/view/translate/translate.js
+++ src/view/translate/translate.js
@@ -1,12 +1,11 @@
-import React from "react"
+import React,{useState} from "react"
 import axios from "axios"
-// import {request} from "../../bin/axiosConfig"
+import md5 from 'js-md5'
 import { Input } from 'antd';
 const { Search } = Input;
 
-
 const Translate = ()=>{
-
+  const [query,setQuery] = useState("")
   const get_baiduFun = (value)=>{
     axios({
       methods:"get",
@@ -15,13 +14,14 @@ const Translate = ()=>{
         q:value,
         from:"en",
         to:"zh",
-        appid:"2015063000000001",
+        appid:"20200929000576935",
         salt:"1435660288",
-        sign:"f89f9594663708c1605f3d736d01d2d4"
+        sign:md5(`20200929000576935${value}1435660288_byVNY9Ujvm4tS3Vxrws`)
       }
     }).then((response)=>{
       console.log("调用了百度")
       console.log(response)
+      setQuery(response.data.trans_result[0].dst)
     })
   }
 
@@ -35,6 +35,9 @@ const Translate = ()=>{
           size="large"
           onSearch={(value)=>get_baiduFun(value)}
         />
+      </div>
+      <div>
+        {query}
       </div>
     </div>
   )
